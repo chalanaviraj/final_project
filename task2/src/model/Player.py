1a7175c8ee6a4f5bc943f7858228a8eaabb19c3b
chalanaviraj <chalanaviraj32341@gmail.com>
2019-10-08 00:29:25 +0500

    task2 with tests

---
+++ task2/src/model/Player.py
@@ -0,0 +1,23 @@
+
+class Player:
+	def __init__(self):
+		self.__score = 0
+		self.__time = 0
+
+	def setName(self, name):
+		self.__name = name
+
+	def getName(self, name):
+		self.__name = name
+
+	def setScore(self, score):
+		self.__score = score
+
+	def getScore(self, score):
+		self.__score = score
+
+	def setTime(self, score):
+		self.__score = score
+
+	def getTime(self, score):
+		self.__score = score
